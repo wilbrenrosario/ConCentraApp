89e11c7e85413307ac924279c1ddb3a7295149ef
wrosario <wrosario@unit.com.do>
2023-08-03 12:56:28 -0400

    Configurando capa aplication, instalando fluent validation y agregando DI

---
+++ Application/Data/IApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Placas;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Data
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Placas> Placas { get; set;}
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
